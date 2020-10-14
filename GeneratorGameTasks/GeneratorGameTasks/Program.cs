d776fe5ad32049a7a139e87d47c921f75b1b0c82
ondrosru <izickov@yandex.ru>
2020-10-14 19:25:46 +0300

    Переработал генератор задач для игры.

---
+++ GeneratorGameTasks/GeneratorGameTasks/Program.cs
@@ -0,0 +1,26 @@
+﻿using GeneratorGameTasks.Types;
+using System;
+
+namespace GeneratorGameTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TaskGenerator taskGenerator = new TaskGenerator();
+            for (int i = 0; i < 20; i++)
+            {
+                Arithmetic3x3 arithmetic3x3 = taskGenerator.GenerateArithmetic3x3();
+                Console.WriteLine(arithmetic3x3.ToString());
+                Console.WriteLine("");
+            }
+            for (int i = 0; i < 20; i++)
+            {
+                Arithmetic4x4 arithmetic4x4 = taskGenerator.GenerateArithmetic4x4();
+                Console.WriteLine(arithmetic4x4.ToString());
+                Console.WriteLine("");
+            }
+        }
+
+    }
+}
